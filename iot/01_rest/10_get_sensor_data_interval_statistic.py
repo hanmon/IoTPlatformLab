9276736388e1335b1348e3534e8e3c31e8ac6198
hanmon <boo.chiu@gmail.com>
2021-04-03 17:30:43 +0800

    added sample code of gettting sensor data in certain interval and statistic

---
+++ iot/01_rest/10_get_sensor_data_interval_statistic.py
@@ -0,0 +1,20 @@
+#讀取設備感測器時間區間內多筆感測資料
+import requests
+import json
+import configparser
+
+sensorId='Temperature'
+config = configparser.ConfigParser()
+config.read('../../cht.conf')
+projectKey = config.get('device-key', 'projectKey')
+deviceId   = config.get('device-key', 'deviceId')
+apiURL = 'http://iot.cht.com.tw/iot/v1/device/' + deviceId + '/sensor/' + sensorId + '/rawdata/statistic'
+#Concate HTTP GET query strings
+apiURL = apiURL + '?' + 'start=2021-03-17T07:00:00Z' + '&end=2021-03-17T09:00:00Z'
+#Change value of my_headers to your project Key
+my_headers = {
+    'CK':projectKey,
+    }
+r=requests.get(apiURL,headers=my_headers)
+print(r.text)
+
