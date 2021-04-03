d3dc3a548edbdb43e009dbd0a26bdbe09bb405a4
hanmon <boo.chiu@gmail.com>
2021-04-03 17:42:32 +0800

    added comment in '09' and '10' sample code

--- iot/01_rest/09_get_sensor_data_interval.py
+++ iot/01_rest/09_get_sensor_data_interval.py
@@ -9,12 +9,15 @@ config.read('../../cht.conf')
 projectKey = config.get('device-key', 'projectKey')
 deviceId   = config.get('device-key', 'deviceId')
 apiURL = 'http://iot.cht.com.tw/iot/v1/device/' + deviceId + '/sensor/' + sensorId + '/rawdata'
-#Concate HTTP GET query strings
+#串接HTTP GET查詢字串，以問號起頭，包括開始與結束時間，格式為 ISO 8601,例如:2016-07-14T23:55:00Z
+#開始與結束時間中間以'&'連接,每次查詢上限為500筆
 apiURL = apiURL + '?' + 'start=2021-03-17T07:00:00Z' + '&end=2021-03-17T09:00:00Z'
-#Change value of my_headers to your project Key
+#改為你自己的projec/device key
 my_headers = {
     'CK':projectKey,
     }
+#送出HTTP GET
 r=requests.get(apiURL,headers=my_headers)
+#印出回應資訊，為JSON格式，可利用linux管線命令導向到jq程式作排版
 print(r.text)
 
--- iot/01_rest/10_get_sensor_data_interval_statistic.py
+++ iot/01_rest/10_get_sensor_data_interval_statistic.py
@@ -1,4 +1,4 @@
-#讀取設備感測器時間區間內多筆感測資料
+#讀取設備感測器時間區間內多筆感測資料統計資訊，包括SUM、COUNT、MIN、MAX與AVG
 import requests
 import json
 import configparser
@@ -9,12 +9,15 @@ config.read('../../cht.conf')
 projectKey = config.get('device-key', 'projectKey')
 deviceId   = config.get('device-key', 'deviceId')
 apiURL = 'http://iot.cht.com.tw/iot/v1/device/' + deviceId + '/sensor/' + sensorId + '/rawdata/statistic'
-#Concate HTTP GET query strings
+#串接HTTP GET查詢字串，包括開始與結束時間，格式為 ISO 8601,例如:2016-07-14T23:55:00Z
+#每次查詢上限為 100,000 筆
 apiURL = apiURL + '?' + 'start=2021-03-17T07:00:00Z' + '&end=2021-03-17T09:00:00Z'
-#Change value of my_headers to your project Key
+#改為你自己的projec/device key
 my_headers = {
     'CK':projectKey,
     }
+#送出HTTP GET
 r=requests.get(apiURL,headers=my_headers)
+#印出回應資訊，為JSON格式，可利用linux管線命令導向到jq程式作排版
 print(r.text)
 
